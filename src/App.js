2f874fb98bc55127ebf38ead9b70b46e7fae3cc5
rajneeshsingh18 <rajneeshsingh9448@gmail.com>
2023-07-16 20:20:10 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 
 import React from "react";
 import './App.css';
-import { Article , Brand , Cta , Feature ,Navbar } from "./components";
-import { Blog , Features , Footer , Header , Possibility , WhatGPT3 } from "./containers";
+import { Article, Brand, Cta, Feature, Navbar } from "./components";
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from "./containers";
 
 function App() {
   return (
     <>
-    <div className="App">
+      <div className="App">
 
-      <div className="gradient__bg">
+        <div className="gradient__bg">
           <Navbar />
           <Header />
+        </div>
+
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <Cta />
+        <Blog />
+        <Footer />
+
+
       </div>
-
-      <Brand/>
-      <WhatGPT3/>
-      <Features/>
-      <Possibility/>
-      <Cta/>
-      <Blog/>
-      <Footer/>
-
-    
-    </div>
     </>
-    
+
   );
 }
 
