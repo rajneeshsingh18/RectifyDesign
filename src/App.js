7dccf6b2b844df72017faa1655f2f70a55f9eb5c
rajneeshsingh18 <rajneeshsingh9448@gmail.com>
2023-07-16 19:11:25 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+
+import React from "react";
 import './App.css';
+import { Article , Brand , Cta , Feature ,Navbar } from "./components";
+import { Blog , Features , Footer , Header , Possibility , WhatGPT3 } from "./containers";
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="gradient__bg">
+          <Navbar />
+          <Header />
+      </div>
+
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <Cta/>
+      <Blog/>
+      <Footer/>
+
+    
     </div>
+    </>
+    
   );
 }
 
